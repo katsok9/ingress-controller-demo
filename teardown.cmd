9c5a9946f27bf5ecbf6a518fdddde067ae30d7cd
katsok9 <katsok9@gmail.com>
2018-02-07 08:12:19 +0200

    Refactor to yaml & add teardown

---
+++ teardown.cmd
@@ -0,0 +1,7 @@
+kubectl delete -f ingress-test.yaml
+kubectl delete -f test-secret.yaml
+kubectl delete -f services.yaml
+kubectl delete -f deployment2.yaml
+kubectl delete -f deployment.yaml
+kubectl delete -f nginx-ingress-rc.yaml
+kubectl delete -f default-server-secret.yaml

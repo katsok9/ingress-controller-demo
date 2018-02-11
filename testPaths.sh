b8fcfdb9577429c07f733f35cd43db19c0f053c4
katsok9 <katsok9@gmail.com>
2018-02-11 18:49:47 +0200

    add testPaths echo

--- testPaths.sh
+++ testPaths.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+echo "-----------------------------"
 echo "testing default backend 404:"
+echo "-----------------------------"
 curl http://my.test.domain/
+echo
 echo "-----------------------------"
 echo "testing html1 path"
+echo "-----------------------------"
 curl http://my.test.domain/html2
 echo "-----------------------------"
 echo "testing html2 path"
+echo "-----------------------------"
 curl http://my.test.domain/html1
